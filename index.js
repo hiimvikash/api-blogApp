7bdc4647feec3e7a757fde49feeae2fc15929ae0
Vikash Gupta <heyimvikash@gmail.com>
2024-03-30 16:28:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ const {checkAuthe} = require('./middlewares/checkAuth');
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL);
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+    }
+    next();
+  });
 app.use(cors({origin: 'https://blogefy.netlify.app', credentials: true }));
 app.use(express.json())
 app.use(cookieParser());

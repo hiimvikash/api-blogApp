47e99968e7fcf493ebe20e65b1702eebadfd7507
Vikash Gupta <heyimvikash@gmail.com>
2024-03-31 11:23:11 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,7 +41,8 @@ async function handleUserLogin(req, res) {
     try {
         const token = await User.matchPasswordAndGenerateToken(username, password);
         // If the user is successfully authenticated, set the token
-        res.cookie("token", token, {sameSite:'None'});
+        res.cookie("token", token, { sameSite: 'None', secure: true });
+
         
 
         const info = validateToken(token);
